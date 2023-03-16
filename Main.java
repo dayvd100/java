c589acf287373281b761c4a839c4ff1d69505be0
dayvd100 <104961896+dayvd100@users.noreply.github.com>
2023-03-16 23:51:25 +0000

    sexo

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+public class Main{
+  public static void main(String[] args){
+    People people = new People();
+    people.setName("Luquinhas");
+    people.setAge(19);
+
+    System.out.println("nome:" + people.getName());
+    System.out.println("idade:" + people.getAge());
   }
 }
